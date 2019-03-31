e3009df6725a4073df29a38c8dba6c4379139315
mohamedhamada2 <mohamedhamada2024@gmail.com>
2019-03-31 16:33:41 +0200

    mohamed

--- app/src/main/java/com/alatheer/shop_peak/Activities/DetailsActivity.java
+++ app/src/main/java/com/alatheer/shop_peak/Activities/DetailsActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alatheer.shop_peak.R;
 
@@ -30,50 +31,50 @@ public class DetailsActivity extends AppCompatActivity {
     }
 
     private void initview() {
-        back_image=findViewById(R.id.back_image);
-        details_img=findViewById(R.id.details_image);
-        ratingBar=findViewById(R.id.ratbar2);
-        details_title=findViewById(R.id.details_title);
-        add_circle=findViewById(R.id.add_circle);
-        remove_circle=findViewById(R.id.remove_circle);
-        counter=findViewById(R.id.counter);
-        shopping_cart=findViewById(R.id.shopping_cart);
-        cart_num=findViewById(R.id.cart_num);
-        addcart=findViewById(R.id.add_cart);
+        back_image = findViewById(R.id.back_image);
+        details_img = findViewById(R.id.details_image);
+        ratingBar = findViewById(R.id.ratbar2);
+        details_title = findViewById(R.id.details_title);
+        add_circle = findViewById(R.id.add_circle);
+        remove_circle = findViewById(R.id.remove_circle);
+        counter = findViewById(R.id.counter);
+        shopping_cart = findViewById(R.id.shopping_cart);
+        cart_num = findViewById(R.id.cart_num);
+        addcart = findViewById(R.id.add_cart);
         //details_des=findViewById(R.id.details_des);
-        details_price=findViewById(R.id.details_price);
-        Intent intent=getIntent();
-        int image=intent.getIntExtra("homeimage",0);
-        String title=intent.getStringExtra("title");
-        String des=intent.getStringExtra("des");
-        String price=intent.getStringExtra("price");
+        details_price = findViewById(R.id.details_price);
+        Intent intent = getIntent();
+        int image = intent.getIntExtra("homeimage", 0);
+        String title = intent.getStringExtra("title");
+        String des = intent.getStringExtra("des");
+        String price = intent.getStringExtra("price");
         details_img.setImageResource(image);
         details_title.setText(title);
-        fab_favorite=findViewById(R.id.fab_favorite);
+        fab_favorite = findViewById(R.id.fab_favorite);
 //        details_des.setText(des);
         details_price.setText(price);
         back_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(DetailsActivity.this,MainActivity.class));
+                startActivity(new Intent(DetailsActivity.this, MainActivity.class));
                 finish();
             }
         });
         add_circle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int count=Integer.parseInt(counter.getText().toString());
+                int count = Integer.parseInt(counter.getText().toString());
                 count++;
-                counter.setText(count++ +"");
+                counter.setText(count++ + "");
             }
         });
         remove_circle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int count=Integer.parseInt(counter.getText().toString());
-                if(count !=0){
+                int count = Integer.parseInt(counter.getText().toString());
+                if (count != 0) {
                     count--;
-                    counter.setText(count-- +"");
+                    counter.setText(count-- + "");
 
                 }
             }
@@ -81,31 +82,33 @@ public class DetailsActivity extends AppCompatActivity {
         addcart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int count_cart =Integer.parseInt(cart_num.getText().toString());
+                int count_cart = Integer.parseInt(cart_num.getText().toString());
                 count_cart++;
-                cart_num.setText(count_cart++ +"");
+                cart_num.setText(count_cart++ + "");
                 cart_num.setVisibility(View.VISIBLE);
             }
         });
         fab_favorite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(flag){
-                    fab_favorite.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_favorite_sold));
-                    flag=false;
-                }else if(!flag){
-                    fab_favorite.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_favorite));
-                    flag=true;
+                if (flag) {
+                    fab_favorite.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_favorite_sold));
+                    flag = false;
+                } else if (!flag) {
+                    fab_favorite.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_favorite));
+                    flag = true;
                 }
             }
         });
         ratingBar.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
+                Toast.makeText(DetailsActivity.this,"helll",Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
+
+
     }
 
 }
